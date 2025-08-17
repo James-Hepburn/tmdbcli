77dc8d72f80eb66b30cd101e859d1046c1915a1f
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-16 20:38:37 -0600

    Model entities

--- src/main/java/com/example/tmdbcli/model/Movie.java
+++ src/main/java/com/example/tmdbcli/model/Movie.java
@@ -1,4 +1,16 @@
 package com.example.tmdbcli.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    private String title;
+    private String releaseDate;
+    private double voteAverage;
+    private int voteCount;
+    private String overview;
 }
--- src/main/java/com/example/tmdbcli/model/TmdbResponse.java
+++ src/main/java/com/example/tmdbcli/model/TmdbResponse.java
@@ -1,4 +1,17 @@
 package com.example.tmdbcli.model;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class TmdbResponse {
+    private int page;
+    private List <Movie> results;
+    private int totalPages;
+    private int totalResults;
 }
