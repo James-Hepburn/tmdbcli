2ba87b7c172cdd536418b6403546f6fb4eb7f523
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-16 20:25:07 -0600

    Initial commit

---
+++ src/main/java/com/example/tmdbcli/TmdbcliApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmdbcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tmdbcli/TmdbcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmdbcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
